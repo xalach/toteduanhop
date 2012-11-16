9c2085d8abca14a3ed3d7b03b9e17629f70e766d
Niv <niv@FlyingInca-Nivtitif.home>
2012-11-16 14:55:30 +0100

    Removed fork for compress

--- projet-system/source/record.c
+++ projet-system/source/record.c
@@ -3,11 +3,7 @@
 
 int compress_archive()
 {
-	if( fork() == 0)
-	{
-		execlp("gzip", "gzip", "fichiertar");
-		ajouter_flag(1);
-	}
+  execlp("gzip", "gzip", "fichiertar");
 }
 
 void ajouter_flag(int bit)
