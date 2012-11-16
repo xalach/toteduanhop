e936997a69fd9e0eaccb84d59e5f6ceb98f0b491
Arnaud Ladoucette <aladouce@emi.u-bordeaux1.fr>
2012-11-16 15:19:30 +0100

    retrait de dossier binaire

--- projet-system/source/Release/main.d
+++
@@ -1,9 +0,0 @@
-main.d: ../main.c ../record.h ../global.h ../out.h ../read.h
-
-../record.h:
-
-../global.h:
-
-../out.h:
-
-../read.h:
--- projet-system/source/Release/read.d
+++
@@ -1,5 +0,0 @@
-read.d: ../read.c ../read.h ../out.h
-
-../read.h:
-
-../out.h:
