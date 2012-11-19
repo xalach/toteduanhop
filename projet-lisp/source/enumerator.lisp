74aff6ed5d15feed2768064024e02562094f33f8
arnaud <arnaud@arnaud.(none)>
2012-11-19 21:16:39 +0100

    ajout de la classe abstract-enumerator et des fonctions généric qui vont avec

---
+++ projet-lisp/source/enumerator.lisp
@@ -0,0 +1,25 @@
+(defclass abstract-enumerator () ())
+
+(defgeneric init-enumerator (enumerator)
+  (:documentation "reinitialise et retourne ENUMERATOR"))
+
+(defgeneric copy-enumerator (enumerator)
+  (:documentation "return une copie reinitialisé de ENUMERATOR"))
+
+(defgeneric next-element-p (enumerator)
+  (:documentation "retourne NIL si il n'y a pas de prochain element, sinon retourne un non NIL"))
+
+(defgeneric next-element (enumerator)
+  (:documentation "retourne le prochain élément et change pour le prochain"))
+
+(defgeneric call-enumerator (enumerator)
+  (:documentation "si il y a un prochain element e, retourne e et T et change pour le prochain element;
+Autrement retourne NIL et NIL"))
+
+(defmethod call-enumerator ((e abstract-enumerator))
+  (if (next-element-p e)
+      (values (next-element e) T)
+      (values (NIL NIL))))
+
+(defmethod init-enumerator ((e abstract-enumerator))
+  e)
