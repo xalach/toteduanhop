3a9b0135abcdc495a24f6c079d34cd575e60d589
Niv <niv@FlyingInca-Nivtitif.home>
2012-11-30 15:56:05 +0100

    XML

--- projet-system/source/xml_creator.c
+++ projet-system/source/xml_creator.c
@@ -1,23 +1,34 @@
-/*#include <libxml/parser.h>
+#include <libxml/parser.h>
 #include <libxml/xmlIO.h>
 #include <libxml/xinclude.h>
 #include <libxml/tree.h>
+#include "global.h"
 #include <stdio.h>
 #include <string.h>
 
-//Ajoute un fichier à l'endroit du pointeur de writer donnée en paramètre
-// -1 si erreur
-int addFile(char *name, file_info infos, xmlTextWriterPtr writer)
+
+xmlNodePtr createXml(char *rootFolder) //Créer le document xml(!= d'un fichier) et retourne un pointeur sur le répertoire racine de l'archive
 {
-  int rc ; //gestion des erreur de xml
-  rc = xmlTextWriterStartElement(writer,name);
-  // rc += xmlTetWriterWriteAttribut(writer,"time",infos.
+  xmlNodePtr nodePtr = xmlNewNode(NULL,rootFolder);
+  xmlDocSetRootElement(xmlNewDoc("1.0"), nodePtr);
+  return (nodePtr);
 }
 
 
-//Ajoute un node de repertoire à l'endroit du pointeur writer 
-int addFolder(char *name, xmlTextWriterPtr writer)
+//Ajoute un fichier au répertoire courant(currentNode) avec les attribut de file_info correspondants.
+void addFile(char *name, struct file_info infos, xmlNodePtr currentNode, char *data)
 {
-
+  xmlNodePtr filePtr = xmlAddChild(currentNode,xmlNewNode(NULL,name));
+  xmlNewChild(filePtr,NULL,"Time",infos.create_time);
+  xmlNewChild(filePtr,NULL,"Mode",infos.mode);
+  xmlNewChild(filePtr,NULL,"Size",infos.size);
+  xmlNewChild(filePtr,NULL,"Data",data);
 }
-*/
+
+
+//Ajoute un répertoire au répertoire courant(currentNode) et retourne un pointeur sur le nouveau répertoire.
+xmlNodePtr addFolder(char *folderName, char *parentNode)
+{
+  //xmlNodePtr currentNode =  
+  return(xmlAddChild(currentNode,xmlNewNode(NULL,folderName)));
+}
