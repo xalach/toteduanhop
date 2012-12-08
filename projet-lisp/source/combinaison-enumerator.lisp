a40ceb8820e9aa9fc1cd70eba243dd3d2d70b107
ltqbx <ltqbx@yahoo.com>
2012-12-08 05:48:58 -0800

    Create combinaison-enumerator.lisp

---
+++ projet-lisp/source/combinaison-enumerator.lisp
@@ -0,0 +1,9 @@
+(defclass combinaision-enumerator (abstract-enumerator) ()
+  (:documentation "énumérateur qui dépend au moins une autre énumérateur"))
+
+(defgeneric sous-enumerators (combinaison-enumerator)
+  (:documentation "les enumerateurs le combinaison-enumerator est dépendant"))
+
+(defmethod init-enumerator :after ((e combinaison-enumerator))
+  (mapc #’init-enumerator
+    (combinaison-enumerators e)))
