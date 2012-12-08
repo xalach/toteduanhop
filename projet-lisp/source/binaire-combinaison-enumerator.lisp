349c2cf7101c5841b9cc325354f94eae052e7e94
ltqbx <ltqbx@yahoo.com>
2012-12-08 06:03:12 -0800

    Create binaire-combinaison-enumerator.lisp

---
+++ projet-lisp/source/binaire-combinaison-enumerator.lisp
@@ -0,0 +1,3 @@
+(defclass binaire-combinaison-enumerator (combinaison-enumerator)
+  ((depends :type list :initarg :depends :reader sous-enumerators))
+  (:documentation "énumérateur qui dépend à plusierus autres énumérateurs"))
