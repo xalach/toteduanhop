6aa0d57d48e1302d45f57e0dbcf24e04ef6754f5
ltqbx <ltqbx@yahoo.com>
2012-12-08 15:57:08 +0100

    Update projet-lisp/source/memo-enumerateur.lisp

--- projet-lisp/source/memo-enumerateur.lisp
+++ projet-lisp/source/memo-enumerateur.lisp
@@ -1,21 +1,21 @@
 (defclass memo-enumerator (unaire-combinaison-enumerator)
-  ((depend-res :accessor depend-res :initform (make-depend-res)))
+  ((depend-result :accessor depend-result :initform (make-depend-result)))
   (:documentation "énumérateur avec une memoire"))
 
 ; Il manque les defgeneric pour certain defmethod
 
 (defmethod make-memo-enumerator ((e abstract-enumerator))
   (init-enumerator
-   (make-instance ’memo-enumerator :enum (copy-enumerator e))))
+   (make-instance ’memo-enumerator :depend (copy-enumerator e))))
 
-(defmethod set-memo-res ((e memo-enumerator))
-  (set-depend-res (depend-res e) (depend e)))
+(defmethod set-memo-result ((e memo-enumerator))
+  (set-depend-result (depend-result e) (depend e)))
 
 (defmethod init-enumerator :after ((e memo-enumerator))
-  (set-memo-res e))
+  (set-memo-result e))
 
-(defmethod trouve-depend ((e memo-enumerateur))
-  (trouve-depend (depend-res e)))
+(defmethod trouve-depend ((e memo-enumerator))
+  (trouve-depend (depend-result e)))
 
 (defmethod objet-depend ((e memo-enumerator))
   (objet-depend (depend-res e)))
