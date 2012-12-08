f28008661792f8dfd012c5f6e64bd1928ce9b2fe
ltqbx <ltqbx@yahoo.com>
2012-12-08 15:34:00 +0100

    Update projet-lisp/source/filtrage-enumerateur.lisp

--- projet-lisp/source/filtrage-enumerateur.lisp
+++ projet-lisp/source/filtrage-enumerateur.lisp
@@ -1,27 +1,27 @@
-(defclass filtrage-enumerateur (unary-relying-enumerateur fun-mixin)
+(defclass filtrage-enumerator (unaire-combinaison-enumerator fun-mixin)
   ())
 
-(defmethod skip-to-next ((e filtrage-enumerateur))
+(defmethod skip-to-next ((e filtrage-enumerator))
   (loop
-     while (enum-found (enum e))
-     until (funcall (fun e) (enum-object (enum e)))
+     while (trouve-depend (depend e))
+     until (funcall (fun e) (objet-depend (depend e)))
     do (set-memo-res (enum e))))
 
 (defmethod init-enumerator :after ((e filtrage-enumerateur))
   (skip-to-next e))
 
 (defmethod next-element-p ((e filtrage-enumerateur))
-  (enum-found (enum e)))
+  (trouve-depend (depend e)))
 
 (defmethod next-element ((e filtrage-enumerator))
   (prog1
-      (enum-object (enum e))
-    (set-memo-res (enum e))
+      (obejet-depend (depend e))
+    (set-memo-res (depend e))
     (skip-to-next e)))
 
 (defmethod make-filtrage-enumerateur ((e abstract-enumerator) filter-fun)
   (init-enumerator
    (make-instance
     ’filtrage-enumerateur
-     :enum (make-memo-enumerateur e)
+     :depend (make-memo-enumerator e)
      :fun filter-fun)))
