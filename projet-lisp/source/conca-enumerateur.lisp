2ee9f262733a92d5c539b0529b33d79689c5cf2b
ltqbx <ltqbx@yahoo.com>
2012-12-08 15:24:44 +0100

    Update projet-lisp/source/conca-enumerateur.lisp

--- projet-lisp/source/conca-enumerateur.lisp
+++ projet-lisp/source/conca-enumerateur.lisp
@@ -1,26 +1,29 @@
-(defclass conca-enumerateur (unary-relying-enumerator)
-  ((next-elements :initform nil :accessor next-elements))
-  )
+(defclass conca-enumerator (unaire-combinaison-enumerator)
+	((next-elements
+	:initform nil
+	:accessor next-elements))
+	(:documentation "concaténation les listes que le sous énuméteur ont énuméré"))
 
-; ou est le defgeneric ?
-(defmethod skip-to-next-non-null ((e conca-enumerateur))
-  (loop
-     until (next-elements e)
-     while (next-element-p (enum e))
-     do (setf (next-elements e)
-	      (next-element (enum e)))))
+(defmethod make-conca-enumerator ((e abstract-enumerator))
+	(init-enumerator
+		(make-instance
+		’append-enumerator
+		:depend (copy-enumerator e))))
 
-(defmethod init-enumerator :after ((e conca-enumerateur))
-  (skip-to-next-non-null e))
+(defmethod skip-to-next-non-null ((e conca-enumerator))
+	(loop
+		until (next-elements e)
+		while (next-element-p (depend e))
+		do (setf (next-elements e)
+						 (next-element (depend e))
 
-(defmethod next-element-p ((e conca-enumerateur))
-  (next-elements e))
+(defmethod init-enumerator :after ((e conca-enumerator))
+	(skip-to-next-non-null e))
 
-(defmethod next-element ((e conca-enumerateur))
-  (prog1
-      (pop (next-elements e))
-    (skip-to-next-non-null e)))
+(defmethod next-element-p ((e conca-enumerator))
+	(next-elements e))
 
-(defmethod make-conca-enumerateur ((e abstract-enumerator))
-  (init-enumerator
-   (make-instance ’conca-enumerateur :enum (copy-enumerator e))))
+(defmethod next-element ((e conca-enumerator))
+	(prog1
+		(pop (next-elements e))
+	(skip-to-next-non-null e)))
