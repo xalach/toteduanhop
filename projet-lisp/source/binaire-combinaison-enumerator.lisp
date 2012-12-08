1cf6f61592ec5fdc602bfa66394d36afecff2a5a
ltqbx <ltqbx@yahoo.com>
2012-12-08 16:25:01 +0100

    Update projet-lisp/source/binaire-combinaison-enumerator.lisp

--- projet-lisp/source/binaire-combinaison-enumerator.lisp
+++ projet-lisp/source/binaire-combinaison-enumerator.lisp
@@ -1,3 +1,3 @@
-(defclass binaire-combinaison-enumerator (combinaison-enumerator)
+(defclass nnaire-combinaison-enumerator (combinaison-enumerator)
   ((depends :type list :initarg :depends :reader sous-enumerators))
   (:documentation "énumérateur qui dépend à plusierus autres énumérateurs"))
