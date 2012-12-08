843e8b1dba27c1c99693d8f8a0a7cf53e830d702
ltqbx <ltqbx@yahoo.com>
2012-12-08 15:04:04 +0100

    Update projet-lisp/source/unaire-combinaison-enumerator.lisp

--- projet-lisp/source/unaire-combinaison-enumerator.lisp
+++ projet-lisp/source/unaire-combinaison-enumerator.lisp
@@ -1,6 +1,6 @@
 (defclass unaire-combinaison-enumerator (combinaison-enumerator)
   ((depend
     :type abstract-enumerator
-    :initarg :enum
-    :reader enum))
+    :initarg :depend
+    :reader depend))
   (:documentation "énumérateur qui dépend à un seul autre énumérateur"))
