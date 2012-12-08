9409d22460090a00eef22cbd21436fa17be809b2
ltqbx <ltqbx@yahoo.com>
2012-12-08 14:25:32 +0100

    Update projet-lisp/source/inductif-enumerateur.lisp

--- projet-lisp/source/inductif-enumerateur.lisp
+++ projet-lisp/source/inductif-enumerateur.lisp
@@ -1,6 +1,9 @@
-
 ; class mixin : defini la fonction d'induction
 ; utiliser par d'autre classe comme filtrage-enumerator
+
+;La fonction f est stocké dans le slot fun apporté par héritage de la classe fun-mixin.
+;QA: C'est juste une classe abstraite (revoir page 131 du poly;)
+
 (defclass fun-mixin ()
   ((fun :initarg :fun :reader fun)))
 
@@ -19,8 +22,16 @@
   e)
 
 ; pourquoi renvoyer toujours vrai ?
+
+;QA: Le slot init-value contient la valeur initiale tandis que le slot current-value détient l'élément suivant à énumérer.
+;Initialement, la valeur actuelle est la valeur initiale.
+;Selon annoncé: une suite (sn)n∈N est d´efinie de mani`ere inductive avec une valeur initiale v et une fonction f qui calcule l’´el´ement suivant `a partir du pr´ec´edent.
+;->ici, on peut toujours énumérer l'élément suivant à partir d'élément précédent, alors c'est normal que ça renvoie toujours vrai
+
 ; si la fonction traite les valeur inferieur à 10
 ; et que current-value est à 12, ça renvoie NIL
+;QA: Ici, c'est une énumérateur simple donc on ne peut pas positionner sur un élément qu'on n'a pas encore crée.
+
 (defmethod next-element-p ((e inductif-enumerateur))
   t)
 
